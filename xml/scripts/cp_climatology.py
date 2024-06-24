bdc6d6848179a50690951e900cf9eb24b14f2220
Utheri Wagura <Utheri.Wagura@an012.princeton.rdhpcs.noaa.gov>
2024-06-24 17:14:13 -0400

    Added scripts and modified common xml to run post processing with FRE

---
+++ xml/scripts/cp_climatology.py
@@ -0,0 +1,8 @@
+import xarray
+import numpy as np
+
+ds = xarray.open_dataset("/work/acr/mom6/nwa12/forecast_output_data/climatology_ocean_month_1993_2019.nc")
+dates = ["2020-9","2020-10","2020-11","2020-12","2021-1","2021-2","2021-3","2021-4","2021-5","2021-6","2021-7","2021-8"]
+ds['time'] = (('lead'),dates)
+ds = ds.swap_dims({'time':'lead'})
+ds.to_netcdf('/work/Utheri.Wagura/extracted/climatoloty_ocean_month_1993_2019.nc')
