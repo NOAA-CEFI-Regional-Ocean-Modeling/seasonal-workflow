ecf0491b3d821e9274fb76192f1c3409ea8146e1
Utheri Wagura <Utheri.Wagura@an104.princeton.rdhpcs.noaa.gov>
2024-07-09 14:29:06 -0400

    Removed hard-coded domain in file name

--- xml/scripts/edit_coordinates.py
+++ xml/scripts/edit_coordinates.py
@@ -15,8 +15,8 @@ model_output_data = Path(config['filesystem']['model_output_data'])
 
 for i in range(1,nens+1):
 	print(f"Working on ensemble member e{i:02d}")
-	ds = xarray.open_dataset(model_output_data / 'extracted'/ args.domain / f'2020-09-e{i:02d}.ocean_month_wrong_coords.nc')
+	ds = xarray.open_dataset(model_output_data / 'extracted'/ args.domain / f'2020-09-e{i:02d}.{args.domain}_wrong_coords.nc')
 	ds = ds.set_coords(['init', 'member'])
 	ds = ds.expand_dims('init')
 	ds = ds.transpose(*(['init', 'time'] + [d for d in ds.dims if d not in ['init', 'lead']]))
-	ds.to_netcdf(model_output_data / 'extracted'/ args.domain / f'2020-09-e{i:02d}.ocean_month.nc', unlimited_dims='init')
+	ds.to_netcdf(model_output_data / 'extracted'/ args.domain / f'2020-09-e{i:02d}.{args.domain}.nc', unlimited_dims='init')
